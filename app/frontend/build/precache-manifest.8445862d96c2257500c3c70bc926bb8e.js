05d8137c369eeeebc794a535be1fcfebbcc23718
Rod Ritter <rod@ritter.co.za>
2019-09-12 04:03:43 +0200

    frontend restructure

---
+++ app/frontend/build/precache-manifest.8445862d96c2257500c3c70bc926bb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17800e3b184de5372d2b50c29203ef3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b969171adec58c892ce6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a47c952a6de75d0f548c",
+    "url": "/static/css/main.218091d4.chunk.css"
+  },
+  {
+    "revision": "b969171adec58c892ce6",
+    "url": "/static/js/2.e3e9ce55.chunk.js"
+  },
+  {
+    "revision": "a47c952a6de75d0f548c",
+    "url": "/static/js/main.9e05bbf9.chunk.js"
+  },
+  {
+    "revision": "5ef87c68040120751bbc",
+    "url": "/static/js/runtime~main.1ab92f27.js"
+  }
+]);
